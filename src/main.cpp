3eb2e326140f76d84bd696d62fec573fe1684bfd
dushyant2006 <dushyant50003@gmail.com>
2026-02-28 16:09:09 +0530

    feat: added doxygen header, defined IR pin and state variable

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,24 @@
 #include <Arduino.h>
+
 /**
  * @file main.cpp
  * @brief Embedded Obstacle Detection System using IR Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Dushyant
+ * @date 2026-02-28
  *
  * @details
  * Reads digital input from IR sensor
  * and detects obstacle presence.
  */
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
+// Define IR sensor digital pin (Use pin 2)
+#define IR_PIN 2
 
- // TODO 2:
- // Create variable to store sensor state
+// Create variable to store sensor state
+int sensorState;
 
 void setup() {
-
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 4:
-    // Configure IR pin as INPUT
-
-    // TODO 5:
-    // Print system initialization message
 }
 
 void loop() {
-
-    // TODO 6:
-    // Read digital value from IR sensor
-
-    // TODO 7:
-    // If obstacle detected
-    //     Print "Obstacle Detected"
-    // Else
-    //     Print "No Obstacle"
-
-    // TODO 8:
-    // Add small delay (200–500ms)
 }
